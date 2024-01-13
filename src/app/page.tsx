9c3411877617597008d83bef49a8cbeb02956874
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-13 08:50:55 -0600

    Added changes to cards for litecoin and btc, there's a strange behavior where the graph is not responsive, but if you shrink the window to the smallest size and refresh the page it is responsive?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function DashboardPage() {
         />
       </div> */}
       <div className="flex-col sm:flex">
-        <div className="border-b">
+        {/* <div className="border-b">
           <div className="flex h-16 items-center px-4">
             <TeamSwitcher />
             <MainNav className="mx-6" />
@@ -57,26 +57,27 @@ export default function DashboardPage() {
               <UserNav />
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-sm font-bold tracking-tight md:text-xl">Dashboard</h2>
             <div className="flex items-center space-x-2">
-              <CalendarDateRangePicker />
-              <Button style={{backgroundColor: "#FF1867", color: "#ffffff"}}>Download</Button>
+              {/* <CalendarDateRangePicker /> */}
+              {/* <Button style={{backgroundColor: "#FF1867", color: "#ffffff"}}>Download</Button> */}
+              <p>Dynamic Date Goes Here</p>
             </div>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="analytics" disabled>
-                Bitcoin
-              </TabsTrigger>
-              <TabsTrigger value="reports" disabled>
+              <TabsTrigger style={{color: "#FF1867"}} value="overview">Bitcoin</TabsTrigger>
+              <TabsTrigger style={{color: "#FF1867"}}value="reports">
                 Ethereum
               </TabsTrigger>
-              <TabsTrigger value="notifications" disabled>
+              <TabsTrigger style={{color: "#FF1867"}} value="notifications">
                 Cardano
+              </TabsTrigger>
+              <TabsTrigger style={{color: "#FF1867"}} value="analytics">
+                Litecoin
               </TabsTrigger>
             </TabsList>
             <TabsContent value="overview" className="space-y-4">
@@ -160,7 +161,7 @@ export default function DashboardPage() {
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
-                      Active Now
+                      Litecoin
                     </CardTitle>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -172,21 +173,22 @@ export default function DashboardPage() {
                       strokeWidth="2"
                       className="h-4 w-4 text-muted-foreground"
                     >
-                      <path style={{color: "#FF1867"}} d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                     
+                      <path style={{color: "#FF1867"}} d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
                     </svg>
                   </CardHeader>
                   <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
+                    <div className="text-2xl font-bold">$573</div>
                     <p className="text-xs text-muted-foreground">
-                      +201 since last hour
+                    +22% from last month
                     </p>
                   </CardContent>
                 </Card>
               </div>
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+              <div className="grid gap-4 md:grid-cols-2">
                 <Card className="col-span-4">
                   <CardHeader>
-                    <CardTitle>Overview</CardTitle>
+                    <CardTitle>Bitcoin</CardTitle>
                   </CardHeader>
                   <CardContent className="pl-2">
                     <Overview />
